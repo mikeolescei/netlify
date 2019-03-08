62ebf17a2f661114078bd0618bb01fa876f86962
Michael Oles <mbo10@pitt.edu>
2019-03-08 11:52:10 -0500

    service to get json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppSettingsService } from './app-settings.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'test-app';
+  constructor(
+    private appSettingsService: AppSettingsService
+) { }
+
+ngOnInit(){
+   this.appSettingsService.getJSON().subscribe(data => {
+        console.log(data);
+    });
+}
 }
